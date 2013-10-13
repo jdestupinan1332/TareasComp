5549245d43f5ef6fb88ad3138f707495493e55bd
JuanD <jd.estupinan1332@uniandes.edu.co>
2013-10-12 19:06:13 -0500

    update some changes that did not upload... oh im screwed

--- Hw_5/punto2.py
+++ Hw_5/punto2.py
@@ -5,6 +5,32 @@ def main():
     """
     Reads sunspots data and calculates its period
     """
-    
+    #define variables
+    inputfile = 'in.out'
+    x = linspace(1,10)
+    tipe = 'cubic'
+
     #read data
-    data = 
+    data = sk.getdata(inputfile)
+
+    #cleans data
+    cleandata(data,x)
+
+    #makes interpolation
+    f = sk.interpol(data, x, tipe)
+    
+    #do fourier
+    fft_x , frecx = sk.fourier(f, data, x)
+
+    #potencies spectrum
+    sqnorm = sk.espectrop(data)
+
+    #erase some data acording to frecuencies
+    sk.erased(frecx, sqnorm)
+    
+    #do reconstruction
+    recondata , reconfrec = sk.reconstruct(sqnorm, frecx)
+    
+    #plot
+    sk.plotdat (recondata, reconfrec)
+
